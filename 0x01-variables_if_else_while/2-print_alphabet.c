703a5f6f26a30bf15097053c698653a0b2a2889f
Yeweds1104 <yewedsdagne@gmail.com>
2022-03-11 20:31:16 -0800

    correcting error

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,15 +12,16 @@ int main(void)
 	int length, i;
 
 	char lower_case[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 
-		'x', 'y', 'z'};
-	
+		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z'};
+
 	length = sizeof(lower_case);
 
-	for (i=0; i<length; i++)
+	for (i = 0; i < length; i++)
 		putchar(lower_case[i]);
+
 	putchar('\n');
 
-	return(0);
-	
+	return (0);
+
 }
