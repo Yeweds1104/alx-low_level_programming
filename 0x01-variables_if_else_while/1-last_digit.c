e9a6acbe9a228a715849eb8f268ad91e5e6ce89e
Yeweds1104 <yewedsdagne@gmail.com>
2022-03-11 18:45:42 -0800

    correcting error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,13 +11,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int last_digit = n % 10;
+	int last_digit; 
+	last_digit = n % 10;
 	if (last_digit > 5)
-		printf("Last digit of ", n ," is ", last_digit, " and  is greater than 5.\n");
-	else if (last_digit == 0)
-		printf("Last digit of ", n ," is ", last_digit, " and is zero.\n");
+		printf("Last digit of %d", n ," is %d", last_digit, " and  is greater than 5.\n")
+		else if (last_digit == 0)
+		printf("Last digit of %d", n ," is %d", last_digit, " and is zero.\n");
 	else if (last_digit < 6 && last_digit != 0)
-		printf("Last digit of ", n ," is ", last_digit, " and is less than 6 and not zero.\n");
+		printf("Last digit of %d", n ," is %d", last_digit, " and is less than 6 and not zero.\n");
 
 	return (0);
 }
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
 	char lower_case[] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
 	length = sizeof(lower_case[]);
 
-	for (int i=0: i<length)
+	for (int i=0: i<length;i++)
+		putchar(lower_case[i]);
+	putchar('\n');
+
+	return(0);
+	
 }
