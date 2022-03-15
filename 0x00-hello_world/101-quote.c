97e3df4dddd67e74b56ec418c816381fbc6e38fc
Yeweds1104 <yewedsdagne@gmail.com>
2022-03-14 20:35:33 -0700

    additional task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+/**
+ * main - prints the given statement
+ * Return: 0 if exited properly
+**/
+int main(void)
+{
+
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (0);
+}
