932b646e72269b5cbeb89ed9f819e55c8533be17
Yeweds1104 <yewedsdagne@gmail.com>
2022-03-22 07:55:46 -0700

    error correction

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,11 +13,10 @@ char *_strcpy(char *dest, char *src)
 
 	i = 0;
 
-	while (*(src + i) != '\0')
-	{
+	do {
 		*(dest + i) = *(src + i);
 		i++;
-	}
+	} while (*(src + i) != '\0');
 
-	return (*dest);
+	return (dest);
 }
