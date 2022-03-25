2cf4800857439409408fd83678e78b2cb8be46cf
Yeweds1104 <yewedsdagne@gmail.com>
2022-03-25 11:46:46 -0700

    error correction

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
 /**
- * _islower - checks if the letter is lowercase or not
- * Return: 1 if it is lowercase, 0 if it is not
- * @c: character to be checked
- */
-
-int _islower(int c)
-{
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
-}
-
-
-/**
  * cap_string - capitalize all words of a string
  * @s: string to be capitalized
  * Return: pointer to capitalized string
