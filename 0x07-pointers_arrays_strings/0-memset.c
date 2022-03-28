c8b447a7f00ae8e84aa689f0fb0f8872afd7d290
Yeweds1104 <yewedsdagne@gmail.com>
2022-03-28 06:01:19 -0700

    more on pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes of the memory pointed by s with b
+ * @s: pointer to a memory
+ * @b: character to be filled
+ * Return: pointer to memory
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is functions of pointers to pointers
